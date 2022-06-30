4959185e69eebe47d5ac34e23ae5d142d1d7ac6b
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-06-30 06:16:20 +0500

    Stars and Forks - Charts Functionality

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -34,28 +34,29 @@ const Repos = () => {
     })
     .slice(0, 5);
 
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13",
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
     },
     {
-      label: "CSS",
-      value: "23",
-    },
-    {
-      label: "JavaScript",
-      value: "80",
-    },
-  ];
+      stars: {},
+      forks: {},
+    }
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
 
   return (
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <Column3D data={chartData} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
-        <Bar3D data={chartData} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
