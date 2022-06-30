d852b5ddc20b6da2e58c2da57b59b316a17398f8
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-07-01 00:54:22 +0500

    Checking Requests Limit

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,9 @@ import { MdSearch } from "react-icons/md";
 import { GithubContext } from "../context/context";
 const Search = () => {
   const [user, setUser] = React.useState("");
-  // get things from global context
+  const { requests } = React.useContext(GithubContext);
+  console.log(requests);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(user);
@@ -25,10 +27,10 @@ const Search = () => {
               value={user}
               onChange={(e) => setUser(e.target.value)}
             />
-            <button type="submit">search</button>
+            {requests > 0 && <button type="submit">search</button>}
           </div>
         </form>
-        <h3>requests : 60 / 60</h3>
+        <h3>requests : {requests} / 60</h3>
       </Wrapper>
     </section>
   );
